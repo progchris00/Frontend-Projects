a7e8bf9c78d6cd2165f35b23d2d19af88617e8e2
progchris00 <prog.christian07@gmail.com>
2024-02-12 17:51:18 +0800

    Implement checkbox answer checking

--- TruthTableExercise/js/main.js
+++ TruthTableExercise/js/main.js
@@ -4,26 +4,63 @@ const cell = document.querySelectorAll("td");
 
 const answers = [
     {
-        part: "one",
-        question_type: "table",
+        part: "2.1",
+        "answer": ["1", "0", "0", "1"],
+
+        part: "2.2",
         "answer": ["1", "0", "0", "1"]
     }
 ]
 
-submitBtn.onclick = checkAnswer;
+submitBtn.onclick = checkAnswerTable;
 
-function checkAnswer()
+// function checkAnswerTable()
+// {
+//     checkAnswerCheckbox(part);
+// }
+
+// function CheckAnswer(part)
+// {
+//     for (let index = 0; index < inputs.length; index++)
+//     {
+//         if (inputs[index].value == tableOneAnswer[index]) {
+//             inputs[index].style.background = "green";
+//             cell[index].style.background = "green";
+//         }
+//         else
+//         {
+//             inputs[index].style.background = "red";
+//             cell[index].style.background = "red";
+//         }
+//     }
+// }
+
+const p1Button = document.querySelector("#p1Button");
+const p1CheckBox = document.querySelectorAll("#p1CheckBox");
+const p1Label = document.querySelectorAll("#p1Label");
+const p1Answers = ["checked", "unchecked", "checked", "unchecked", "checked"];
+
+p1Button.onclick = checkAnswerCheckbox;
+
+function checkAnswerCheckbox()
 {
-    for (let index = 0; index < inputs.length; index++)
+    for (let index = 0; index < p1CheckBox.length; index++)
     {
-        if (inputs[index].value == tableOneAnswer[index]) {
-            inputs[index].style.background = "green";
-            cell[index].style.background = "green";
-        }
-        else
+        if (p1CheckBox[index].checked && p1Answers[index] == "checked")
         {
-            inputs[index].style.background = "red";
-            cell[index].style.background = "red";
+            p1Label[index].style.color = "green";
+        }
+        else if (p1CheckBox[index].checked && p1Answers[index] == "unchecked")
+        {
+            p1Label[index].style.color = "red";
+        }
+        else if (!p1CheckBox[index].checked && p1Answers[index] == "checked")
+        {
+            p1Label[index].style.color = "red";
+        }
+        else if (!p1CheckBox[index].checked && p1Answers[index] == "unchecked")
+        {
+            p1Label[index].style.color = "green";
         }
     }
 }
